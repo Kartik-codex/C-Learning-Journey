17be0d915a77953ce11c77f556c0e524639ef346
Kartik-codex <doyouknnowkartikcodes@gmail.com>
2026-07-09 02:52:13 +0530

    Create 03-even-numbers-infinite.c

---
+++ 03-even-numbers-infinite.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //Press Ctrl + C to stop
+    printf("Even Numbers till infinity \n");
+
+    int n = 2;
+     
+    while (1)
+    {
+        printf("%d\n" , n);
+        n += 2;
+
+    }
+
+    return 0;
+}
